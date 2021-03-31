d93309376d53ee8311672e45540713a82d53a03c
Emily Davis <emilyjdavis94@gmail.com>
2021-03-30 22:44:31 -0400

    Begin react test to show plant collection

---
+++ react-app/src/PlantCollection.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+describe('PlantCollection', () => {
+    it('displays the names of the plants in the collection', () => {
+        // feels like here is where i'd want to mock the data coming from the server
+        expect(true).toBe(false);
+    });
+});
