d48b6937f0f0c9622a2de6658239b0e46647345e
Emily Davis <emilyjdavis94@gmail.com>
2021-04-07 19:55:34 -0400

    Fix jacked up state typing

--- react-app/src/PlantCollection.tsx
+++ react-app/src/PlantCollection.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import axios from 'axios';
 
+type Plant = {
+    name: string;
+};
+
 const PlantCollection = () => {
-    const [plantList, setPlantList] = React.useState(null);
+    const [plantList, setPlantList] = React.useState<Plant[]>([]);
 
     React.useEffect(() => {
         const fetchData = async () => {
             const res = await axios.get('/plants');
-            await setPlantList(res.data);
+            const stuff = res.data.map((d: any) => ({ name: d.name }));
+            await setPlantList(stuff);
         }
         fetchData();
       }, []);
 
     return (
         <div data-testid='plant-collection'>
-            { plantList &&
-             (plantList.map(
-                (plant, i) => (<div key={i}> {plant.name} </div>)))
-            }
+            {plantList.map((d, i) => <div key = {i}> {d.name} </div>)}
         </div>
     );
 };
